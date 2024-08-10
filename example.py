976bea570a5fb9560d4fee75a0894a76f3842874
Impossibum <86384743+Impossibum@users.noreply.github.com>
2024-08-09 23:47:05 -0400

    Making comment more accurate

--- example.py
+++ example.py
@@ -7,7 +7,7 @@ done = False
 for i in range(10):
     env.reset()
     while not done:
-        # sampling a move from the handy list of legal moves as sampling from action space would cause illegal moves
+        # sampling a move from the handy list of legal moves as sampling from action space could cause illegal moves
         action = sample(env.legal_moves, 1)[0]
         obs, reward, done, info = env.step(action)
         # if match is concluded, print the results
